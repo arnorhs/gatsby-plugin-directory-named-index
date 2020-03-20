e2b4e3b365d0633cd19c081e073ff71e4daa966a
arnorhs <arnorhs@gmail.com>
2020-03-20 00:52:09 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# gatsby-plugin-directory-named-index
+
+A gatsby plugin wrapping
+[directory-named-webpack-plugin](https://github.com/shaketbaby/directory-named-webpack-plugin)
+
+allowing you to have the following file structure:
+
+`src/components/MyComp/MyComp.js`
+
+and yet import your files like:
+
+```javascript
+import MyComp from '../src/components/MyComp'
+```
+
+making editor tab management and clarity simple without cluttering
+your file tree with unnecessary stuff.
+
+### License
+
+MIT
+
---
+++ src/gatsby-node.js
@@ -0,0 +1,24 @@
+const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin')
+
+exports.onCreateWebpackConfig = ({ stage, actions }, { include, ...opts }) => {
+  const options = {
+    honorIndex: false,
+    exclude: [
+      path.resolve('./src/pages'),
+      /node_modules/
+    ],
+    include: [
+      path.resolve('./src'),
+      ...(include || [])
+    ],
+    ...opts
+  }
+
+  actions.setWebpackConfig({
+    resolve: {
+      plugins: [
+        new DirectoryNamedWebpackPlugin(options)
+      ]
+    }
+  })
+}
