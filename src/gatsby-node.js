192ba5113c69376661a5a14f25125927453d3e52
David Dostal <david.dostal1112@gmail.com>
2020-06-16 13:18:37 +0200

    Fix issue #1 (missing 'path' module)
    Require the nodejs `path` module to fix the `path is not defined` error thrown in `onCreateWebpackConfig` in `gatsby-node.js`.

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin')
 
 exports.onCreateWebpackConfig = ({ stage, actions }, { include, ...opts }) => {
